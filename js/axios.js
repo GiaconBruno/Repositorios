ec4075a6dee0d50e019936dab3b74ab93f428eb4
GiaconBruno <56009821+GiaconBruno@users.noreply.github.com>
2020-04-05 15:44:32 -0300

    Initial commit

---
+++ js/axios.js
@@ -0,0 +1,72 @@
+//CRIA FUNCAO ASSINCRONA
+const runAxios = async () => {
+    //CRIA ELEMENTO DE ESPERAR CARREGAR
+    document.querySelector('#news').innerHTML = `<div class='p-3 mt-5'>
+                                                    <h3>Carregando </h3>
+                                                    <div class='spinner-grow text-success'></div> <div class='spinner-grow text-success'></div> <div class='spinner-grow text-success'></div>
+                                                </div>`;
+    await dados();
+};
+runAxios(); //EXECUTA FUNCAO ASSINCRONA
+
+//CRIA FUNCAO GET (BUSCAR DADOS)
+function dados() {
+    var cont = 0; //VARIAVEL CONTADOR
+    var fullW = window.innerWidth;
+    axios.get('https://api.github.com/repositories')
+        .then(function (response) {
+            //VARRE TODOS OS DADOS
+            response.data.forEach(value => {
+                var { name, description, html_url, owner: { avatar_url } } = value;
+
+                //REMOVER LINKS
+                var first = String(description).indexOf('http://');
+                var end = String(description).indexOf('.com');
+                var clear = String(description).slice(first, end + 4);
+                description = String(description).replace(clear, "");
+
+                //VERIFICA CONTEUDO MAIOR QUE O PERMITIDO
+                if (fullW <= 1000) {
+                    if (description.length > 135) {
+                        description = description.substr(0, 135) + "...";
+                    } else {
+                        description = description.substr(0, 133);
+                    }
+                } else {
+                    description = String(description).substr(0, 300);
+                }
+
+                //VERIFICA FIRST REGISTRO
+                const content = document.querySelector('#news');
+                if (cont === 0) {
+                    content.innerHTML = '';
+                }
+
+                //CRIA ELEMENTO DIV
+                const div = document.createElement("div");
+                //DETERMINA QUE DIV EH ELEMENTO FILHO DE ELEMENTO #NEWS
+                content.appendChild(div);
+
+                //CRIA CONTEUDO
+                const full = `<a href="${html_url}" target="_black">
+                                <div class="news-bord m-2 text-left d-flex">
+                                    <img class="p-2" src="${avatar_url}" />
+                                    <div class="text-left p-2 pr-3 conteudo">
+                                        <div>
+                                            <h4><strong>${name}</strong></h4>
+                                        </div>
+                                        <div>
+                                            <p class="text-justify">${description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>`;
+                //SALVA CONTEUDO DENTRO DO ELEMENTO DIV
+                div.innerHTML = full;
+                cont++;//PASSA CONTADOR
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+function position() {
+    var fullH = window.innerHeight;
+    var posh = document.querySelector('#section-contact [class=container]').offsetTop;
+    var posw = document.querySelector('#section-contact ul').offsetLeft;
+    var elem = document.querySelector('#news');
+
+    var height = (posh / fullH) * 100;
+
+    elem.style.top = height + 10 + 'vh';
+    elem.style.left = posw + 'px';
+    elem.style.right = posw + 'px';
+    elem.style.bottom = ((height - 2) * -1) + 'vh';
+}
