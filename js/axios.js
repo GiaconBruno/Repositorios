3e015e9befb7c462afd369db8831801f1188529a
GiaconBruno <56009821+GiaconBruno@users.noreply.github.com>
2020-04-11 18:16:39 -0300

    att

--- js/axios.js
+++ js/axios.js
@@ -4,7 +4,7 @@ var tempo = null;
 
 function search() {
     clearTimeout(tempo);
-    tempo = setTimeout(function() {
+    tempo = setTimeout(function () {
         console.log(tempo);
 
         repos = document.querySelector('input').value;
@@ -14,7 +14,7 @@ function search() {
 }
 
 //CRIA FUNCAO ASSINCRONA
-const runAxios = async() => {
+const runAxios = async () => {
     //CRIA ELEMENTO DE ESPERAR CARREGAR
     document.querySelector('#news').innerHTML = `<div class='p-3 mt-5'>
                                                     <h3>Carregando 
@@ -24,15 +24,15 @@ const runAxios = async() => {
                                                     </h3>
                                                 </div>`;
     //CRIA SPINNERs COM DALEY
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('#dv1').classList = "spinner-grow text-success";
     }, 0);
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('#dv2').classList = "spinner-grow text-success";
     }, 125);
-    setTimeout(function() {
+    setTimeout(function () {
         document.querySelector('#dv3').classList = "spinner-grow text-success";
-    }, 200);
+    }, 1000);
 
     await dados();
 };
@@ -42,35 +42,34 @@ function dados() {
     var cont = 0; //VARIAVEL CONTADOR
     var fullW = window.innerWidth;
     axios.get(`https://api.github.com/users/${repos}/repos`)
-        .then(function(response) {
+        .then(function (response) {
             if (response.status === 200) {
                 //VARRE TODOS OS DADOS
                 response.data.forEach(value => {
-                    var { name, description, html_url, has_pages, owner: { avatar_url } } = value;
+                    var { name, description, html_url, has_pages, owner: { avatar_url }, updated_at } = value;
 
-                    // //REMOVER LINKS
-                    // var first = String(description).indexOf('http://');
-                    // var end = String(description).indexOf('.com');
-                    // var clear = String(description).slice(first, end + 4);
-                    // description = String(description).replace(clear, "");
+                    //Formatando Data
+                    updated_at = updated_at.substr(0, 10);
+                    updated_at = updated_at.slice(8, 10) + "/" + updated_at.slice(5, 7) + "/" + updated_at.slice(0, 4);
 
-                    var link = `https://${repos}.github.io/${name}`;
                     //VERIFICA SE EXISTE GIT-PAGES
-                    has_pages == true ? link : link = html_url;
+                    var icon, link;
+                    (has_pages === true) ? icon = 'fa-check' : icon = 'fa-times negative';
+                    (has_pages === true) ? link = `https://${repos}.github.io/${name}` : link = `https://github.com//${repos}/${name}`;
 
                     //VERIFICA CONTEUDO MAIOR QUE O PERMITIDO
                     description === null ? description = "Sem descrição" : description;
 
                     if (fullW <= 1000) {
-                        (description.length > 200) ? description: description.substr(0, 200);
-                        (description.length <= 200) ? description: description.substr(0, 200) + "...";
+                        (description.length > 200) ? description : description.substr(0, 200);
+                        (description.length <= 200) ? description : description.substr(0, 200) + "...";
                     } else {
                         description = String(description).substr(0, 300);
                     }
 
                     //VERIFICA FIRST REGISTRO
                     const content = document.querySelector('#news');
-                    (cont === 0) ? content.innerHTML = '': content.innerHTML;
+                    (cont === 0) ? content.innerHTML = '' : content.innerHTML;
 
                     //CRIA ELEMENTO DIV
                     const div = document.createElement("div");
@@ -80,13 +79,13 @@ function dados() {
                     //CRIA CONTEUDO
                     const full = `<a href="${link}" target="_black">
                                 <div class="news-bord m-2 text-left d-flex">
-                                    <img class="p-2" src="${avatar_url}" />
-                                    <div class="text-left p-2 pr-3 conteudo text-break">
-                                        <div>
-                                            <h4><strong>${name}</strong></h4>
-                                        </div>
-                                        <div>
-                                            <p class="text-justify">${description}</p>
+                                    <img class="p-2 align-self-center" src="${avatar_url}" />
+                                    <div class="text-left py-3 pr-2 conteudo text-break">
+                                        <span class="d-flow text-right">${updated_at} </span>
+                                        <i class="mr-2 mt-2 float-left text-success fas ${icon} fa-lg"></i>
+                                        <span class="text-break mt-3"><strong>${name}</strong></span>
+                                        <div class="d-block">
+                                            <span class="text-justify text-break">${description}</span>
                                         </div>
                                     </div>
                                 </div>
@@ -97,10 +96,10 @@ function dados() {
                 });
             }
         })
-        .catch(function(error) {
+        .catch(function (error) {
             //VALIDA SE EXISTE
             console.log(error.message);
-            (error.message) === "Request failed with status code 404" ? fail("Repositorio não encontrado!!"): fail(error);
+            (error.message) === "Request failed with status code 404" ? fail("Repositorio não encontrado!!") : fail(error);
         });
 }
 
